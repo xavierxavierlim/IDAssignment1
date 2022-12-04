8251a79b5bfd8e9fd0b98c644f0e6c15a7adad4e
Xavier Lim <s10242016@connect.np.edu.sg>
2022-12-04 16:34:25 +0800

    added comments for all files

--- script.js
+++ script.js
@@ -13,8 +13,8 @@ function showSlides(n) {
   let i; 
   let slides = document.getElementsByClassName("mySlides"); //get all the elements with the class "mySlides"
   let dots = document.getElementsByClassName("dot"); //get all the elements with the class "dot"
-  if (n > slides.length) {slideIndex = 1}  //if exceed the last slide after pressing next button go back to the first slide  
-  if (n < 1) {slideIndex = slides.length}  //brings user back to last page from first slide if they press the "left" button 
+  if (n > slides.length) {slideIndex = 1}  //go back to the first slide if the current one is the last in the slideshow 
+  if (n < 1) {slideIndex = slides.length}  //go to the last slide if the current one is the fist in the slideshow
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";  //hide element without deleting it
   }
