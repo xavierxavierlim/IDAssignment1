0df7674ed2944c6fc1515362fa54f702f7762af9
Xavier Lim <s10242016@connect.np.edu.sg>
2022-12-04 14:38:01 +0800

    minor style changes

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
 let slideIndex = 1;
-showSlides(slideIndex);
+showSlides(slideIndex); //starts off with showSlides(1)
 
-function plusSlides(n) {
+function plusSlides(n) { //go to the next slide
   showSlides(slideIndex += n);
 }
 
-function currentSlide(n) {
+function currentSlide(n) { //set showSlides(n) to the current slide user is on
   showSlides(slideIndex = n);
 }
 
 function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
+  let i; 
+  let slides = document.getElementsByClassName("mySlides"); //get all the elements with the class "mySlides"
+  let dots = document.getElementsByClassName("dot"); //get all the elements with the class "dot"
+  if (n > slides.length) {slideIndex = 1}  //if exceed the last slide after pressing next button go back to the first slide  
+  if (n < 1) {slideIndex = slides.length}  //brings user back to last page from first slide if they press the "left" button 
   for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
+    slides[i].style.display = "none";  //hide element without deleting it
   }
   for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
+    dots[i].className = dots[i].className.replace("active", ""); //change classname to blank for all the elements with dots
   }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
+  slides[slideIndex-1].style.display = "block";  //start on a new line
+  dots[slideIndex-1].className += " active";     //add class "active" which changes background colour
 }
